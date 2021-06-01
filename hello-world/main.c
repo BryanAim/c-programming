a4c5aba65423c924b0dc73e5c8f3d23aaea8e799
Isale <isale.bryan@gmail.com>
2021-06-01 11:21:30 +0300

    first commit

---
+++ hello-world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char characterName[] = "Aim";
+  int characterAge = 56;
+   printf("My name is %s\n", characterName);
+   printf("my age is %d years.\n", characterAge);
+
+   return 0;
+}

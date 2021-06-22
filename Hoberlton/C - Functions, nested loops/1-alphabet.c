44db33bb69167984b9eee166babce8d43dafb2df
Isale <isale.bryan@gmail.com>
2021-06-22 11:58:29 +0300

    alphabet print

---
+++ Hoberlton/C - Functions, nested loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Function that prints alphabet in lowercase
+ *
+ * Return: Void
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	a='a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+
+	_putchar('\n');
+	
+}

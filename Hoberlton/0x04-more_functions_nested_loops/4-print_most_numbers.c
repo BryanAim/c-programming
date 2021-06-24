50b794415e0afdd326d8d1e10c605b43d2d08e82
Isale <isale.bryan@gmail.com>
2021-06-24 16:32:54 +0300

    print most numbers except some

---
+++ Hoberlton/0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Prints all numbers except for the ones given.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+  int num;
+
+  for (num = '0'; num <= '9'; num++)
+  {
+    if (num != '2' && num != '4')
+    {
+      _putchar(num);
+    }
+    _putchar('\n');
+  }
+}
